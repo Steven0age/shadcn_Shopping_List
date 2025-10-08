1e6d03b7dfdd7ecc60fcc332f2f8bdc3925bbbee
Stephan Haak <info@stephan-haak.com>
2025-10-08 08:27:03 +0200

    add gh action

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/shadcn_Shopping_List/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
